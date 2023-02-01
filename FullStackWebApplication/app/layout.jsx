6fe1fbf9eba1036ace8c0822decc1f0af93a78c0
smasnb12 <smasnb12@gmail.com>
2023-02-01 15:36:26 +0500

    intial

---
+++ FullStackWebApplication/app/layout.jsx
@@ -0,0 +1,32 @@
+import "./globals.css";
+import {Montserrat} from "@next/font/google"
+const montserrat = Montserrat ({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: "--font-montserrat",
+})
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body className= {`${montserrat.className} max-32 my-12`}>
+        {/* <nav>
+          <h1>logo</h1>
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Sign up</a>
+            </li>
+          </ul>
+        </nav> */}
+        {children}
+      </body>
+    </html>
+  );
+}
