f9470d7e615395212f62af5c28b172a43a1fa411
smasnb12 <smasnb12@gmail.com>
2023-02-19 00:30:48 +0500

    todo app

--- todoapp/app/page.tsx
+++ todoapp/app/page.tsx
@@ -1,129 +1,15 @@
+"use client";
 
-"use client"
-
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react";
 import CheckWordsLength from "./check-words-length";
 import CounterApp from "./counter-app";
 import CounterAppAdvance from "./counter-app-advance";
 import Todo from "./todo";
 
-
-
-
 export default function Home() {
-  // const val = "Hello"
-  // const textareaRef = useRef<HTMLTextAreaElement | null>(null);
-  // const [val3, setVal3] = useState<string>()
-  // const [val2, setVal2] = useState("Hello world2")
-  // const textAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-  //   setVal3(event.target.value);
-  // };
-  // useEffect(()=> {if (textareaRef && textareaRef.current) {
-  //   textareaRef.current.style.height = "0px";
-  //   const scrollHeight = textareaRef.current.scrollHeight;
-  //   textareaRef.current.style.height = scrollHeight + "px"}}, [val3]);
-  // const onChangeHandler2 = (e:any)=>{
-  //   console.log("e is", e.target.value)
-  //   setVal2(e.target.value)
-  // }
-  // const [val, setVal] = useState("Hello World!")
-  // const onChangeHandler = (e: any)=> {
-  //   console.log ("e is", e.target.value)
-  //   setVal(e.target.value)
-
-  
   return (
-  <> 
-  
-  {/* <input type= "text" placeholder='Hello World'  />  place holder are used to
-  show value in shaded */}
-   {/* <input type="text" value= {val} onClick={onChangeHandler} />   */}
-   {/* <input type={"text"} value = {val} onChange = {onChangeHandler} />
-   <br />
-   <input type= "text" value={val2} onChange = {onChangeHandler2} />
-   <br />
-   Length of first string: {val.length}
-   <br />
-   Length of second string: {val2.length}  */}
-    
- 
-   <div>
-   
-   {/* <textarea
-        ref={textareaRef}
-        style={styles.textareaDefaultStyle}
-        onChange={textAreaChange}
-      ></textarea> */}
-    <br />
-    {/* <input type="text" value={val} onChange = {onChangeHandler} /> */}
-   
-    <br />
-    {/* <input type="text" value={val2} onChange ={onChangeHandler2} /> */}
-    <br />
-    {/* Length of words in text with spaces: {val.length} */}
-    <br />
-    {/* Length of words in textarea with spaces: {val3?.length} */}
-    <br />
-    {/* Length of words in textarea without spaces: {val3?.replace(/\s/g, "").length} */}
-    <br />
-    {/* Length of words in text without spaces: {val.replace(/\s/g, "").length} */}
-    <br />
-    {/* Length of scond string: {val2.length} */}
-    <br />
-    {/* <Todo /> */}
-    <Todo />
-    <br />
-    <h1>Count unlimited Words with spaces or without spaces</h1>
-    <h2>Write or paste text in textarea</h2>
-    <br />
-    <CheckWordsLength />
-    <br />
-    <h1>Counter App</h1>
-    <br />
-    <CounterApp />
-    <br />
-    <h1>Counter App Advance</h1>
-    <br />
-    <CounterAppAdvance />
-   </div>
-  </>)
+    <div>
+      <Todo />
+    </div>
+  );
 }
-const styles: { [name: string]: React.CSSProperties } = {
-  container: {
-    marginTop: 50,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  textareaDefaultStyle: {
-    padding: 5,
-    width: 400,
-    display: "block",
-    resize: "none",
-    backgroundColor: "#eee",
-  },
-};
-// "use client"
-// import { useState } from 'react';
-
-// type Todo = {
-//   id: number;
-//   title: string;
-//   isComplete: boolean;
-// };
-
-// const useTodo = (): [Todo[], (todo: Todo) => void, (id: number) => void] => {
-//   const [todos, setTodos] = useState<Todo[]>([]);
-
-//   const addTodo = (todo: Todo) => {
-//     setTodos([...todos, todo]);
-//   };
-
-//   const removeTodo = (id: number) => {
-//     setTodos(todos.filter(todo => todo.id !== id));
-//   };
-
-//   return [todos, addTodo, removeTodo];
-// };
-
-// export default useTodo;
--- todoapp/app/todo.tsx
+++ todoapp/app/todo.tsx
@@ -1,139 +1,225 @@
-"use client";
+// "use client";
+
+// import { useState } from "react";
+
+// export default function Todo() {
+//   const [todo, setTodo] = useState("");
+//   // const [editingTodo, setEditingTodo] = useState("")
+//   const [todos, setTodos] = useState([
+//     { todoText: String ("Sign In"), completed: Boolean (false) },
+//     { todoText: String("Register"), completed: Boolean (false) },
+//     { todoText: String("CheckWordsLength"), completed: Boolean (true) },
+//     { todoText: String("check CounterApp"), completed: Boolean (false) },
+//     { todoText: String("check CounterAppAdvance"), completed: Boolean (true) },
+//   ]);
+//   const onClickHandler = (myElm: any) => {
+//     console.log("My elm", myElm);
+
+//     // map runs on array and return an array
+//     const newTodos = todos.map((todo) => {
+//       if (todo.todoText == myElm.todoText) {
+//         todo.completed = !todo.completed;
+//       }
+//       return todo;
+//     });
+//     console.log(newTodos);
+
+//     setTodos(newTodos);
+//   };
+//   const addTodo = () => {
+//     const newTodo = { todoText: todo, completed: false };
+//     // const newTodos = { ...todos, newTodo };
+//     const newTodos = [...todos, newTodo]
+//     setTodos(newTodos);
+//     setTodo("");
+//   };
+//   // const editTodo = (todoText: string) => {
+//   //   setEditingTodo(todoText);
+//   // };
+//   // const updateTodo = () => {
+//   //   const newTodos = todos.map((todo) => {
+//   //     if (todo.todoText === editingTodo) {
+//   //          todo.todoText = todo;
+//   //     }
+//   //     return todo;
+//   //   })
+//   //   setTodos(newTodos);
+//   //   setEditingTodo("");
+//   // }
+//   const deleteTodo = (myTodo:any)=>{
+//     const newTodos = todos.filter((todo)=>{
+//       if(todo.todoText == myTodo.todoText) return false;
+//       return true;
+//     });
+//     console.log("old todos", todos, "new todos", newTodos);
+//     setTodos(newTodos)
+//     // const deleteTodo = (meraTodo: any) => {
+//     //   const newTodos = todos.filter((todo) => {
+//     //     if (todo.todoText == meraTodo.todoText) return false;
+//     //     return true;
+//     //   });
+//     //   console.log("old todos", todos, "new todos", newTodos);
+//     //   setTodos(newTodos);
+//   }
+//   // const editTodo = () => {
+//   //   const newTodos = todos.toString((todo)=>{
+//   //     return
+//   //   })
+//   // }
+//   return (
+//     <>
+//       <div>
+//         <h1>Todos</h1>
+//       </div>
+//       <input
+//         placeholder="add todo tex"
+//         value={todo}
+//         onChange={(e) => {
+//           setTodo(e.target.value);
+//         }}
+//       />
+//       <button onClick={addTodo}>Add</button>
+//       <ul>
+//         {/* <li>Sign In</li>
+//         <li>Register</li>
+//         <li>CheckWordsLength</li>
+//         <li>check CounterApp</li>
+//         <li>check CounterAppAdvance</li> */}
+//         {/* <li>{todos[0]}</li>
+//         <li>{todos[1]}</li>
+//         <li>{todos[2]}</li>
+//         <li>{todos[3]}</li>
+//         <li>{todos[4]}</li> */}
+//         {todos.map((elm) => {
+//           return (
+//             <li
+//               style={{
+//                 color: elm.completed ? "green" : "orange",
+//                 fontStyle: "oblique",
+//                 listStyle: "none",
+//               }}
+//               key={elm.todoText}
+//             >
+//               <input
+//                 type="checkbox"
+//                 checked={elm.completed}
+//                 onChange={() => {
+//                   onClickHandler(elm);
+//                 }}
+//               />
+//               {elm.todoText}
+//               {/* <button onClick={()=>{updateTodo(elm)}}>Edit</button> */}
+//               <button onClick={()=>{deleteTodo(elm)}}
+//               /*style={{backgroundColor: "red", margin: "10", 
+//                display: "inline-block"}}*/
+//                >
+//                 {" "}
+//                 Delete</button>
+//                 {/* <button onClick={()=>{
+//                   editTodo(elm)
+//                 }}
+//                 // style={{backgroundColor: "darkcyan"}}
+//                  >
+//                   {" "}
+//                   Edit Todo</button> */}
+//                   {/* <input
+//             placeholder="edit todo text"
+//             value={todo}
+//             onChange={(e) => {
+//               setTodo(e.target.value);
+//             }}
+//           />
+//           <button onClick={updateTodo}>Update</button> */}
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     </>
+//   );
+// }
 
 import { useState } from "react";
 
 export default function Todo() {
   const [todo, setTodo] = useState("");
-  // const [editingTodo, setEditingTodo] = useState("")
+  const [editingTodo, setEditingTodo] = useState("");
   const [todos, setTodos] = useState([
-    { todoText: String ("Sign In"), completed: Boolean (false) },
-    { todoText: String("Register"), completed: Boolean (false) },
-    { todoText: String("CheckWordsLength"), completed: Boolean (true) },
-    { todoText: String("check CounterApp"), completed: Boolean (false) },
-    { todoText: String("check CounterAppAdvance"), completed: Boolean (true) },
+    { todoText: "Breakfast", completed: false },
+    { todoText: "Lunch", completed: false },
+    { todoText: "Super", completed: true },
+    { todoText: "Dinner", completed: false },
+    { todoText: "Candle light dinner", completed: true },
   ]);
-  const onClickHandler = (myElm: any) => {
-    console.log("My elm", myElm);
 
-    // map runs on array and return an array
+  const onClickHandler = (myElm: any) => {
     const newTodos = todos.map((todo) => {
-      if (todo.todoText == myElm.todoText) {
+      if (todo.todoText === myElm.todoText) {
         todo.completed = !todo.completed;
       }
       return todo;
     });
-    console.log(newTodos);
-
     setTodos(newTodos);
   };
+
   const addTodo = () => {
     const newTodo = { todoText: todo, completed: false };
-    // const newTodos = { ...todos, newTodo };
     const newTodos = [...todos, newTodo]
     setTodos(newTodos);
     setTodo("");
   };
-  // const editTodo = (todoText: string) => {
-  //   setEditingTodo(todoText);
-  // };
-  // const updateTodo = () => {
-  //   const newTodos = todos.map((todo) => {
-  //     if (todo.todoText === editingTodo) {
-  //          todo.todoText = todo;
-  //     }
-  //     return todo;
-  //   })
-  //   setTodos(newTodos);
-  //   setEditingTodo("");
-  // }
+
   const deleteTodo = (myTodo:any)=>{
     const newTodos = todos.filter((todo)=>{
-      if(todo.todoText == myTodo.todoText) return false;
+      if(todo.todoText === myTodo.todoText) return false;
       return true;
     });
-    console.log("old todos", todos, "new todos", newTodos);
     setTodos(newTodos)
-    // const deleteTodo = (meraTodo: any) => {
-    //   const newTodos = todos.filter((todo) => {
-    //     if (todo.todoText == meraTodo.todoText) return false;
-    //     return true;
-    //   });
-    //   console.log("old todos", todos, "new todos", newTodos);
-    //   setTodos(newTodos);
-  }
-  // const editTodo = () => {
-  //   const newTodos = todos.toString((todo)=>{
-  //     return
-  //   })
-  // }
+  };
+
   return (
-    <>
-      <div>
-        <h1>Todos</h1>
+    <div 
+
+    className="container mx-auto mt-10" 
+    // className="container mx-auto mt-10" 
+    >
+      <h1 className="text-6xl font-bold text-blue-700 px-4 text-center ">Add Todos Here</h1>
+      <div className="flex mb-4 pt-10" >
+        <input 
+          className="border rounded-l px-3 py-2 w-full border-purple-600 "
+          placeholder="add todo text"
+          value={todo}
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+        />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r" onClick={addTodo}>
+          Add
+        </button>
       </div>
-      <input
-        placeholder="add todo tex"
-        value={todo}
-        onChange={(e) => {
-          setTodo(e.target.value);
-        }}
-      />
-      <button onClick={addTodo}>Add</button>
-      <ul>
-        {/* <li>Sign In</li>
-        <li>Register</li>
-        <li>CheckWordsLength</li>
-        <li>check CounterApp</li>
-        <li>check CounterAppAdvance</li> */}
-        {/* <li>{todos[0]}</li>
-        <li>{todos[1]}</li>
-        <li>{todos[2]}</li>
-        <li>{todos[3]}</li>
-        <li>{todos[4]}</li> */}
+      <ul className="list-disc list-inside">
         {todos.map((elm) => {
           return (
             <li
-              style={{
-                color: elm.completed ? "green" : "orange",
-                fontStyle: "oblique",
-                listStyle: "none",
-              }}
+              className={`py-2 ${elm.completed ? "text-green-500 line-through" : "text-orange-500"}`}
               key={elm.todoText}
             >
               <input
                 type="checkbox"
+                className="mr-2"
                 checked={elm.completed}
                 onChange={() => {
                   onClickHandler(elm);
                 }}
               />
               {elm.todoText}
-              {/* <button onClick={()=>{updateTodo(elm)}}>Edit</button> */}
-              <button onClick={()=>{deleteTodo(elm)}}
-              /*style={{backgroundColor: "red", margin: "10", 
-               display: "inline-block"}}*/
-               >
-                {" "}
-                Delete</button>
-                {/* <button onClick={()=>{
-                  editTodo(elm)
-                }}
-                // style={{backgroundColor: "darkcyan"}}
-                 >
-                  {" "}
-                  Edit Todo</button> */}
-                  {/* <input
-            placeholder="edit todo text"
-            value={todo}
-            onChange={(e) => {
-              setTodo(e.target.value);
-            }}
-          />
-          <button onClick={updateTodo}>Update</button> */}
+              <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 ml-4 rounded" onClick={()=>{deleteTodo(elm)}}>
+                Delete
+              </button>
             </li>
           );
         })}
       </ul>
-    </>
+    </div>
   );
 }
+
